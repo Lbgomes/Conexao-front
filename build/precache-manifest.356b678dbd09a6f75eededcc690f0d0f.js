1e74f7b92aa8cc712d012c74a48454b9337548c5
Murilo Gomes <murilolbgomes@gmail.com>
2021-02-02 10:31:48 -0300

    add build

---
+++ build/precache-manifest.356b678dbd09a6f75eededcc690f0d0f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce880fb3fedefff6ca521b2149101b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87b7a1ebe98f589606b5",
+    "url": "/static/css/main.c0ac53ab.chunk.css"
+  },
+  {
+    "revision": "1823b47861540941b30d",
+    "url": "/static/js/2.ee9cab56.chunk.js"
+  },
+  {
+    "revision": "dd4bb6b1c1a592900e4d436c81b54d9d",
+    "url": "/static/js/2.ee9cab56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87b7a1ebe98f589606b5",
+    "url": "/static/js/main.48bbea02.chunk.js"
+  },
+  {
+    "revision": "879a5d5cb82cf8d6c6df",
+    "url": "/static/js/runtime-main.fd536e40.js"
+  },
+  {
+    "revision": "7d50ecb75d0e1a1540264a3023c08e2c",
+    "url": "/static/media/add.7d50ecb7.svg"
+  },
+  {
+    "revision": "6d7c26615252e9851b164c9c23206115",
+    "url": "/static/media/building-line.6d7c2661.svg"
+  },
+  {
+    "revision": "7f451701598f8f9dc1570d262e0462d5",
+    "url": "/static/media/dashboard.7f451701.svg"
+  },
+  {
+    "revision": "977430b6fc2ca486f3ce333fca949eae",
+    "url": "/static/media/default-candidato.977430b6.png"
+  },
+  {
+    "revision": "3ab0565cf3a38e7c761390110b5ac43f",
+    "url": "/static/media/default-empresa.3ab0565c.png"
+  },
+  {
+    "revision": "1a5c38e30967ec228cc34bd781522dbc",
+    "url": "/static/media/erro.1a5c38e3.jpg"
+  },
+  {
+    "revision": "8007d741ad6672b97ba01b663353c5f9",
+    "url": "/static/media/historico.8007d741.svg"
+  },
+  {
+    "revision": "405513d64db7f8ad3a19a825dae88de7",
+    "url": "/static/media/image-13@1x.405513d6.jpg"
+  },
+  {
+    "revision": "eee6fc7f201bdbaa1838513287ae8bf3",
+    "url": "/static/media/imagem2.eee6fc7f.jpg"
+  },
+  {
+    "revision": "6c345a621ddc126ae697e65780078ff7",
+    "url": "/static/media/login-circle-line.6c345a62.svg"
+  },
+  {
+    "revision": "98f49b911cc4fdafcc5c2fb79fe554f2",
+    "url": "/static/media/logo-dark.98f49b91.png"
+  },
+  {
+    "revision": "5ee1460a67742840e54f96c846401ac2",
+    "url": "/static/media/logo-senai-principal.5ee1460a.png"
+  },
+  {
+    "revision": "84620de00f9fd8f1a8dce57fae2f7063",
+    "url": "/static/media/logout.84620de0.svg"
+  },
+  {
+    "revision": "ce0ba1dcca83f5fb422dca22ae2286a7",
+    "url": "/static/media/search-line.ce0ba1dc.svg"
+  },
+  {
+    "revision": "61749379d7a78b1d5139f3f2b5f2855c",
+    "url": "/static/media/user-3-line.61749379.svg"
+  },
+  {
+    "revision": "bb3b9fa36bf136594625d1e3189b7e6b",
+    "url": "/static/media/user-add-line.bb3b9fa3.svg"
+  },
+  {
+    "revision": "debf124a86f809032aee1c1b10c77d13",
+    "url": "/static/media/vagas.debf124a.svg"
+  }
+]);
